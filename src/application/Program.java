839ca92a92e6d6aa2c8bab5c598aee1bbab7a5e6
ghlimaservicos <ghlima@gmail.com>
2022-05-04 08:32:34 -0300

    ajustando

--- src/application/Program.java
+++ src/application/Program.java
@@ -3,9 +3,6 @@ package application;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
-import java.util.Locale;
-import java.util.Scanner;
-import entities.Product;
 
 public class Program {
 	public static void main(String[] args) throws ParseException {
