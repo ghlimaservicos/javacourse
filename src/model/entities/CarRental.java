0a0a11e4df9f2d2c690d25b4eab829eb6fa15200
ghlimaservicos <ghlima@gmail.com>
2022-05-10 18:08:32 -0300

    car Rental example

--- src/model/entities/CarRental.java
+++ src/model/entities/CarRental.java
@@ -1,5 +1,71 @@
 package model.entities;
 
+import java.util.Date;
+
 public class CarRental {
 
+	private Date start;
+	private Date finish;
+	
+	private Vehicle vehicle;
+	private Invoice invoice;
+	
+	
+	public CarRental() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+
+	public CarRental(Date start, Date finish, Vehicle vehicle) {
+		super();
+		this.start = start;
+		this.finish = finish;
+		this.vehicle = vehicle;
+	}
+
+
+	public Date getStart() {
+		return start;
+	}
+
+
+	public void setStart(Date start) {
+		this.start = start;
+	}
+
+
+	public Date getFinish() {
+		return finish;
+	}
+
+
+	public void setFinish(Date finish) {
+		this.finish = finish;
+	}
+
+
+	public Vehicle getVehicle() {
+		return vehicle;
+	}
+
+
+	public void setVehicle(Vehicle vehicle) {
+		this.vehicle = vehicle;
+	}
+
+
+	public Invoice getInvoice() {
+		return invoice;
+	}
+
+
+	public void setInvoice(Invoice invoice) {
+		this.invoice = invoice;
+	}
+	
+	
+	
+	
+	
 }
--- src/model/services/RentalServices.java
+++ src/model/services/RentalServices.java
@@ -1,9 +1,27 @@
 package model.services;
 
+import model.entities.CarRental;
+
 public class RentalServices {
+	
+	private Double pricePerDay;
+	private Double pricePerHour;
+	
+	private BrazilTaxService taxService;
 
 	public RentalServices() {
 		// TODO Auto-generated constructor stub
 	}
 
+	public RentalServices(Double pricePerDay, Double pricePerHour, BrazilTaxService taxService) {
+		super();
+		this.pricePerDay = pricePerDay;
+		this.pricePerHour = pricePerHour;
+		this.taxService = taxService;
+	}
+	
+	public void processInvoice(CarRental carRental) {
+		
+	}
+	
 }
